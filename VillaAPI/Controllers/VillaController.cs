0519da8824b19726ceb50bfcc0d311374cad34da
ahmed hariedy <113946682+batman-2006@users.noreply.github.com>
2024-08-08 01:37:33 +0300

    Add project files.

---
+++ VillaAPI/Controllers/VillaController.cs
@@ -0,0 +1,21 @@
+﻿    using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VillaAPI.Models;
+
+namespace VillaAPI.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>()
+            {
+                new Villa() {Id = 1 , Name ="Pool View"},
+                new Villa() {Id = 2 , Name="Beach View"}
+            };
+        }
+    }
+}
